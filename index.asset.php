06326a2a0623498e817cf95fc33c9dc6a6cc5550
Nahidul-Islam-Sayel <islamnahidul0825@gmail.com>
2024-04-25 09:27:41 +0600

    styble post-carousel

---
+++ index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '8a7280eea9ce54ec8adf');
